343fd0e51de01ddfa9a63db75ecfb76d8929c989
EDA <eda@mac-mini-1.local>
2016-09-14 11:29:34 +1200

    tidied index

--- index.js
+++ index.js
@@ -15,26 +15,12 @@ app.set('views', path.join(__dirname + '/views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-// List all wombles
 app.get('/', routes.getAll)
-
-// List wombles with their characteristics
 app.get('/outfit', routes.getOutfits)
-
-// List rubbish duties
 app.get('/trash', routes.getDuties)
-
-// Add a new womble
 app.post('/newWomble', routes.newWomble)
-
-// Delete womble
 app.get('/delete', routes.delWomble)
-
-// Update womble
 app.post('/update', routes.updateWom)
-
-
-
 
 var PORT = 3000
 
